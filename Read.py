24e54717b3891654ea5f6b9e927ada3abbe11725
test <test@neto.com>
2022-10-20 07:09:48 +0300

    1. Пустой файл

---
+++ Read.py
@@ -0,0 +1,75 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
+f = open('test.txt')
+res = f.readlines()
+print(res)
+f.close()
+
+f = open('test.txt')
+res = f.read()
+print(res)
+f.close()
+
+# ************************
+
+f2 = open('folder/123/test3.txt')
+res2 = f2.read()
+print(type(res2))
+print(res2)
+
+# **************************
+
+f2 = open(r'D:\_PYTHON\файлы\fol\test4.txt')
+res2 = f2.read()
+print(res2)
+
+# ***************************
+
+f2 = open('testik.txt', encoding='utf8')
+res2 = f2.read()
+print(res2)
+
+# *****************************
+
+f2 = open(r'D:\_PYTHON\файлы\fol\test4.txt')
+
+with open ('test.txt', 'w') as a:
+    a.write('10')
+    a.write('1112')
+
+with open ('test.txt', 'w') as a:
+    a.writelines(['10\n', '12\n', '13'])
+
+with open ('test.txt', 'a') as a:
+    a.writelines(['\nпривет'])
+
+"""
+
+
+
+
+
